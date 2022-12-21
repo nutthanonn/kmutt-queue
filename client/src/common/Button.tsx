34aafbabc9569e1fa62dc64ee14ed8b8225348c6
nutthanon <nutthanon.tho@gmail.com>
2022-12-22 02:12:00 +0700

    style: SEO website

--- client/src/common/Button.tsx
+++ client/src/common/Button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
 const Button: React.FC<ButtonProps> = (props) => {
   return (
     <button
-      className="mx-2 border-[0.5px]  hover:(border-[#9499ff]) px-5 py-2 duration-[250ms] ease rounded-md flex items-center"
+      className="mx-2 border-[0.5px] hover:(border-[#9499ff]) px-5 py-2 duration-[250ms] ease rounded-md flex items-center"
       onClick={props.onClick}
     >
       {props.children}
--- client/src/components/dashboard/CountQueue.tsx
+++ client/src/components/dashboard/CountQueue.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import Typography from "../../common/Typography";
 
 interface CountQueueProps {
-  currentQueue: number;
   myQueue: number;
 }
 
 const CountQueue: React.FC<CountQueueProps> = (props) => {
   return (
     <div className="px-20 inline-flex gap-10">
-      <Typography type="h4">Current Queue : {props.currentQueue}</Typography>
       <Typography type="h4">My Queue : {props.myQueue}</Typography>
     </div>
   );
