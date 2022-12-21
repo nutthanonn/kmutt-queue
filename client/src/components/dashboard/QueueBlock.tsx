6b6edae5f4d181293a4b7a6951ec65507c1924ee
nutthanon <nutthanon.tho@gmail.com>
2022-12-22 02:13:59 +0700

    feat(connect-socketIO)

--- client/src/components/dashboard/QueueBlock.tsx
+++ client/src/components/dashboard/QueueBlock.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import Avatar from "./Avatar";
+import { v4 as uuidv4 } from "uuid";
 
-const QueueBlock: React.FC = () => {
-  const myQueue: number = 3;
+interface QueueBlockProps {
+  totalQueue: number;
+  myQueue: number;
+}
+
+const QueueBlock: React.FC<QueueBlockProps> = (props) => {
+  const { totalQueue, myQueue } = props;
 
   return (
     <div className="min-h-[40vh] border-1 mt-10 flex items-center px-20 overflow-auto">
       <div className="flex gap-x-10 items-center">
-        {Array.from(Array(30).keys()).map((item) => {
-          return <Avatar item={item} myQueue={myQueue} />;
+        {Array.from(Array(totalQueue).keys()).map((item) => {
+          return <Avatar item={item + 1} myQueue={myQueue} key={uuidv4()} />;
         })}
       </div>
     </div>
