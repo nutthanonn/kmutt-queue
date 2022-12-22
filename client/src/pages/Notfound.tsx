16a4350340f6de4b40033399abd51871792ced23
nutthanon <nutthanon.tho@gmail.com>
2022-12-22 15:47:15 +0700

    feat(component): not found page

---
+++ client/src/pages/Notfound.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Typography from "../common/Typography";
+
+const Notfound: React.FC = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <Typography type="h1">404 Not Found</Typography>
+    </div>
+  );
+};
+
+export default Notfound;
