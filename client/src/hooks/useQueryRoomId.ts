c70f0344fc349a0b64921fd546c4ac1d99caeb23
nutthanon <nutthanon.tho@gmail.com>
2022-12-22 15:46:51 +0700

    feat(hook): useQueryRoom

---
+++ client/src/hooks/useQueryRoomId.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { useSearchParams, useNavigate } from "react-router-dom";
+import { fetchCheckRoom } from "../api/fetchCheckRoom";
+import { socket } from "../config/socketConfig";
+import { MicrosoftProfile } from "../interfaces/MicrosoftProfile";
+
+export const useQueryRoomId = (profile: MicrosoftProfile) => {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const [roomId, setRoomId] = useState<string>("");
+
+  useEffect(() => {
+    const query = searchParams.get("q");
+    if (!query) {
+      navigate("/");
+    } else {
+      async function checkRoom() {
+        const res = await fetchCheckRoom(query as string);
+        console.log(res.status);
+
+        if (res.status) {
+          socket.emit("enqueue", { ...profile, roomId: query });
+          navigate("/dashboard");
+        } else {
+          navigate("/notfound");
+        }
+      }
+
+      checkRoom();
+
+      setRoomId(query);
+    }
+  }, [searchParams, navigate]);
+
+  return { roomId };
+};
