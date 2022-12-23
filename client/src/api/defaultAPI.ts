8e7249263b9e4489c1604f3fa9f03f5842e3ad36
nutthanon <nutthanon.tho@gmail.com>
2022-12-23 14:29:15 +0700

    fix(api): router

--- client/src/api/defaultAPI.ts
+++ client/src/api/defaultAPI.ts
@@ -1,2 +1,2 @@
-export const DEFAULT_API = "https://kmutt-queue-backend.onrender.com/";
+export const DEFAULT_API = "https://kmutt-queue-backend.onrender.com";
 // export const DEFAULT_API = "http://localhost:8000";
